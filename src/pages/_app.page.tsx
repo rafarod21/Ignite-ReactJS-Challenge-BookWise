45746786d3257088d4a3fff55d83552bfa6eaa3d
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2023-03-29 18:24:03 -0300

    Initial commit

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,18 @@
+// _app.tsx
+
+import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+
+import { globalStyles } from '../styles/global'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+globalStyles()
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={nunito.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
